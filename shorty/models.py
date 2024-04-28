6b0904be77ea6bdfb4dc551a48e0acbff964de0c
jad <alaoui@umich.edu>
2024-04-28 18:27:57 -0400

    Basic Functionality

--- shorty/models.py
+++ shorty/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class URL(models.Model):
+    
+    shortURL = models.CharField(max_length=50, primary_key=True)
+    longURL = models.CharField(max_length=50)
--- shorty/views.py
+++ shorty/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import URL
+from django.template import loader
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+import requests
+import json
+
+def shortenURL(longURL):
+    api_url = "https://smolurl.com/api/links"
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    data = json.dumps({"url": longURL})
+    response = requests.post(api_url, headers=headers, data=data)
+    if response.status_code == 201:
+        result = response.json()
+        entry = URL.objects.create(
+            shortURL=result['data']['short_url'],
+            longURL=result['data']['destination_url']
+        )
+        return {
+            "short_url": result['data']['short_url'],
+            "destination_url": result['data']['destination_url']
+        }
+    else:
+        return "Error: " + response.text
+
+def lengthenURL(shortURL):
+    try:
+        longURL = URL.objects.get(shortURL=shortURL)
+        return {"short_url": shortURL, "destination_url": longURL.longURL}
+    except URL.DoesNotExist:
+        return "Error: URL not found"
+
+def index(request):
+    print(loader.get_template('shorty/index.html'))
+    return render(request,'shorty/index.html')
+
+class ShortenAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        long_url = request.data.get('url')
+        if not long_url:
+            return Response({'error': 'URL is required'}, status=status.HTTP_400_BAD_REQUEST)
+        result = shortenURL(long_url)
+        print(result)
+        return Response(result, status=status.HTTP_201_CREATED if 'short_url' in result else status.HTTP_400_BAD_REQUEST)
+
+class LengthenAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        short_url = request.data.get('short_url')
+        if not short_url:
+            return Response({'error': 'Short URL is required'}, status=status.HTTP_400_BAD_REQUEST)
+        result = lengthenURL(short_url)
+        return Response(result, status=status.HTTP_200_OK if 'destination_url' in result else status.HTTP_404_NOT_FOUND)
